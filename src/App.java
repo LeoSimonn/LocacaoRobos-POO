69b9687a4b14a01fb5f4d226b4ebcfa74ed1ff47
Leonardo <leonardo.monteiro004@edu.pucrs.br>
2024-06-24 10:23:43 -0300

    Refactor App.java to use cadastrarRobos.fxml for UI

--- src/App.java
+++ src/App.java
@@ -1,26 +1,19 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 public class App extends Application {
-    public static void main(String[] args) throws Exception {
-        launch(args);
-    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("cadastrarRobos.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene tela = new Scene(root);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(tela);
+        Parent root = FXMLLoader.load(getClass().getResource("cadastrarRobos.fxml"));
+        primaryStage.setTitle("Cadastro de Robôs");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
